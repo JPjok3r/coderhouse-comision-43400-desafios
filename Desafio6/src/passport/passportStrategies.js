a44cd24489bec591667f38faf7c495bf95777de4
JuanSaavedra <juan.ps.a@hotmail.com>
2023-09-25 15:09:55 -0400

    Desafio Hashing y Passport

--- Desafio6/src/passport/passportStrategies.js
+++ Desafio6/src/passport/passportStrategies.js
@@ -1,5 +1,5 @@
 import passport from 'passport';
-import { userModel } from '../../../Desafio5/src/dao/db/models/user.model.js';
+import { userModel } from '../dao/db/models/user.model.js';
 import { Strategy as LocalStrategy } from 'passport-local';
 import { Strategy as GithubStrategy } from 'passport-github2';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
